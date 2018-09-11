b4207ccb277a1c41f6b919f491071a2aa463db17
Robert Gao <gaorober@usc.edu>
2018-09-10 20:01:16 -0700

    updated index to reflect components

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,24 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
+import Header from './header.jsx';
+import Overview from './overview.jsx';
+import Menu from './menu.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      data: {},
     };
   }
 
-  componentDidMount() {
+  componentWillMount() {
     axios.get('/restaurants/30590734/overview')
-    .then(data => {console.log(data)});
+    .then(data => {
+      console.log(data.data[0]);
+      this.setState({
+        data: data.data[0]
+      })
+    });
   }
   
   render() {
     return (
       <div>
-        <h1>Hello World</h1>
+        <h1>{this.state.data.name}</h1>
+        <Header headerData={this.state.data.header}/>
+        <div>
+          {this.state.data.description}
+        </div>
+        <Overview />
+        <Menu />
       </div>
     );
   }
