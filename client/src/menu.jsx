ec6991a5e9dbb4323b445943ec1a3d7cefc694bc
Robert Gao <gaorober@usc.edu>
2018-09-10 20:00:53 -0700

    created menu component

---
+++ client/src/menu.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Menu extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render () {
+    return (
+      <div>
+        <h2>Menu</h2>
+      </div>
+    )
+  }
+}
+
+export default Menu;
