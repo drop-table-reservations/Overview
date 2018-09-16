df13f2b4451682fcd7ceab14aae9640080a747b3
Robert Gao <gaorober@usc.edu>
2018-09-16 13:16:58 -0700

    refactors rendering of details for less clutter and more readability

--- client/src/overview.jsx
+++ client/src/overview.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Description from './description';
 
-class Overview extends React.Component {
+class Overview extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -17,157 +17,78 @@ class Overview extends React.Component {
     });
   }
 
+  createDetails() {
+    const { info } = this.props;
+    const { collapsed } = this.state;
+    const leftContent = [
+      [info.privateDining, 'fa fa-coffee', 'Private Dining'],
+      [info.diningStyle, 'fa fa-glass', 'Dining Style'],
+      [info.cuisines, 'fa fa-cutlery', 'Cuisines'],
+      [info.hoursOfOperation, 'fa fa-clock-o', 'Hours of Operation'],
+      [info.phoneNumber, 'fa fa-phone', 'Phone Number'],
+      [info.website, 'fa fa-external-link', 'Website'],
+      [info.paymentOptions, 'fa fa-credit-card', 'Payment Options'],
+      [info.dressCode, 'fa fa-user-secret', 'Dress Code'],
+      [info.executiveChef, 'fa fa-github-alt', 'Executive Chef'],
+      [info.catering, 'fa fa-birthday-cake', 'Catering'],
+      [info.specialEvents, 'fa fa-calendar-plus-o', 'Special Events'],
+    ];
+
+    const rightContent = [
+      [info.openGoogleMaps, 'fa fa-map-o', 'Open Google Maps'],
+      [info.Neighborhood, 'fa fa-building-o', 'Neighborhood'],
+      [info.crossStreet, 'fa fa-road', 'Cross Street'],
+      [info.parkingDetails, 'fa fa-product-hunt', 'Parking Details'],
+      [info.publicTransit, 'fa fa-bus', 'Public Transit'],
+      [info.giftCards, 'fa fa-gift', 'Gift Cards'],
+      [info.entertainment, 'fa fa-music', 'Entertainment'],
+      [info.additional, 'fa fa-tags', 'Additional'],
+    ];
+
+    const mapper = array => (
+      array.map((detail) => {
+        const content = detail[0];
+        const iconName = detail[1];
+        const title = detail[2];
+        if (content) {
+          return (
+            <div className="detail-box">
+              <i className={iconName} />
+              <div className="detail">
+                <div className="detail-title">{title}</div>
+                <div>{content}</div>
+              </div>
+            </div>
+          );
+        }
+        return <div />;
+      }));
+
+    return (
+      <div className={collapsed ? 'overview-collapsed' : 'overview-content'}>
+        <div className="leftOverview">
+          {mapper(leftContent)}
+        </div>
+        <div className="rightOverview">
+          {mapper(rightContent)}
+        </div>
+      </div>
+    );
+  }
+
   renderButton() {
     const { collapsed } = this.state;
     return collapsed
-      ? <button type="button" className="expand-overview" onClick={() => this.handleExpansion()}>View All Details</button>
+      ? <button type="button" className="expand-button" onClick={() => this.handleExpansion()}>View All Details</button>
       : <div />;
   }
 
   render() {
     const { info } = this.props;
-    const { collapsed } = this.state;
     return (
       <div className="overview">
         <Description description={info.description} />
-        <div className={collapsed ? 'overview-collapsed' : 'overview-content'}>
-          <div className="leftOverview">
-            <div className="detail-box">
-              <i className="fa fa-glass" />
-              <div className="detail">
-                <div className="detail-title">Private Dining</div>
-                <div className="dining">{info.privateDining}</div>
-              </div>
-            </div>
-            <div className="detail-box">
-              <i className="fa fa-coffee" />
-              <div className="detail">
-                <div className="detail-title">Dining Style</div>
-                {info.diningStyle}
-              </div>
-            </div>
-            <div className="detail-box">
-              <i className="fa fa-cutlery" />
-              <div className="detail">
-                <div className="detail-title">Cuisines</div>
-                {info.cuisines}
-              </div>
-            </div>
-            <div className="detail-box">
-              <i className="fa fa-clock-o" />
-              <div className="detail">
-                <div className="detail-title">Hours of Operation</div>
-                {info.hoursOfOperation}
-              </div>
-            </div>
-            <div className="detail-box">
-              <i className="fa fa-phone" />
-              <div className="detail">
-                <div className="detail-title">Phone Number</div>
-                {info.phoneNumber}
-              </div>
-            </div>
-            <div className="detail-box">
-              <i className="fa fa-external-link" />
-              <div className="detail">
-                <div className="detail-title">Website</div>
-                {info.website}
-              </div>
-            </div>
-            <div className="detail-box">
-              <i className="fa fa-credit-card" />
-              <div className="detail">
-                <div className="detail-title">Payment Options</div>
-                {info.paymentOptions}
-              </div>
-            </div>
-            <div className="detail-box">
-              <i className="fa fa-user-secret" />
-              <div className="detail">
-                <div className="detail-title">Dress Code</div>
-                {info.dressCode}
-              </div>
-            </div>
-            <div className="detail-box">
-              <i className="fa fa-github-alt" />
-              <div className="detail">
-                <div className="detail-title">Executive Chef</div>
-                {info.executiveChef}
-              </div>
-            </div>
-            <div className="detail-box">
-              <i className="fa fa-birthday-cake" />
-              <div className="detail">
-                <div className="detail-title">Catering</div>
-                {info.catering}
-              </div>
-            </div>
-            <div className="detail-box">
-              <i className="fa fa-calendar-plus-o" />
-              <div className="detail">
-                <div className="detail-title">Special Events*</div>
-                {info.specialEvents}
-              </div>
-            </div>
-          </div>
-          <div className="right-overview">
-            <div className="detail-box">
-              <i className="fa fa-map-o" />
-              <div className="detail">
-                <div className="detail-title map-address">{info.openGoogleMaps}</div>
-              </div>
-            </div>
-            <div className="detail-box">
-              <i className="fa fa-building-o" />
-              <div className="detail">
-                <div className="detail-title">Neighborhood</div>
-                {info.Neighborhood}
-              </div>
-            </div>
-            <div className="detail-box">
-              <i className="fa fa-road" />
-              <div className="detail">
-                <div className="detail-title">Cross Streets</div>
-                {info.crossStreet}
-              </div>
-            </div>
-            <div className="detail-box">
-              <i className="fa fa-product-hunt" />
-              <div className="detail">
-                <div className="detail-title">Parking Details</div>
-                {info.parkingDetails}
-              </div>
-            </div>
-            <div className="detail-box">
-              <i className="fa fa-bus" />
-              <div className="detail">
-                <div className="detail-title">Public Transit*</div>
-                {info.publicTransit}
-              </div>
-            </div>
-            <div className="detail-box">
-              <i className="fa fa-gift" />
-              <div className="detail">
-                <div className="detail-title">Gift Cards*</div>
-                {info.giftCards}
-              </div>
-            </div>
-            <div className="detail-box">
-              <i className="fa fa-music" />
-              <div className="detail">
-                <div className="detail-title">Entertainment*</div>
-                {info.entertainment}
-              </div>
-            </div>
-            <div className="detail-box">
-              <i className="fa fa-tags" />
-              <div className="detail">
-                <div className="detail-title">Additional</div>
-                {info.additional}
-              </div>
-            </div>
-          </div>
-        </div>
+        {this.createDetails()}
         <div>{this.renderButton()}</div>
       </div>
     );
