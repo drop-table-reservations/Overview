1b7a87fcd7a4efad0ea6a8044e95cdca0c0c1aed
Robert Gao <gaorober@usc.edu>
2018-09-20 20:14:17 -0700

    wraps map component in a class to prevent rerendering upon state change

--- client/src/googleMaps.jsx
+++ client/src/googleMaps.jsx
@@ -2,13 +2,26 @@ import React from 'react';
 import MapWithAMarker from './MapWithAMarker';
 import googleAPI from '../../googleAPI';
 
-const GoogleMaps = () => (
-  <MapWithAMarker
-    googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${googleAPI.key}&v=3.exp&libraries=geometry,drawing,places`}
-    loadingElement={<div style={{ height: '100%' }} />}
-    containerElement={<div style={{ height: '100%' }} />}
-    mapElement={<div style={{ height: '240px', width: '310px' }} />}
-  />
-);
+class GoogleMaps extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  shouldComponentUpdate() {
+    return false;
+  }
+
+  render() {
+    return (
+      <MapWithAMarker
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${googleAPI.key}&v=3.exp&libraries=geometry,drawing,places`}
+        loadingElement={<div style={{ height: '100%' }} />}
+        containerElement={<div style={{ height: '100%' }} />}
+        mapElement={<div style={{ height: '240px', width: '310px' }} />}
+      />
+    );
+  }
+}
 
 export default GoogleMaps;
